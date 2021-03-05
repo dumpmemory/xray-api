9f4eeaf304771496f4fb0df7335b43ed59813002
zcmimi <zcmimi@outlook.com>
2021-03-06 02:14:11 +0800

    .

--- init.sh
+++ init.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-wget https://github.com/v2fly/xray-core/releases/download/v4.34.0/xray-linux-64.zip
-unzip -o xray-linux-64.zip -d main/xray-core
-rm xray-linux-64.zip
-go get github.com/xtls/xray-core v1.3.0
-go get google.golang.org/grpc v1.35.0
-go get github.com/pires/go-proxyproto
-go get github.com/seiflotfy/cuckoofilter
-go get github.com/akkuman/parseConfig
-go get github.com/gin-gonic/gin
-
+wget https://github.com/XTLS/Xray-core/releases/download/v1.3.1/Xray-linux-64.zip
+unzip -o Xray-linux-64.zip -d xray-core
+rm Xray-linux-64.zip
+go mod download
+go get -u
 git clone git@github.com:zcmimi/xray-api-release.git expt
