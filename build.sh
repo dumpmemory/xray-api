1e5e6447630906d2b7f380c21acaa775187ede89
zcmimi <zcmimi@outlook.com>
2021-05-29 12:17:22 +0800

    .

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cp xray-core release/xray-core -r
 cp config.yaml release/config.yaml
 cp config.yaml release/config.default.yaml
 go build -o release/xray-api .
+rm xray-api.zip
+zip xray-api.zip -r release
